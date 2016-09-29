4bb68cbc442b59249857a83676fbe0e9009ff6ba
Wei Ma <weima.gm@gmail.com>
2016-09-29 00:32:52 -0700

    Solved a few koans

--- test/01_about_streams.js
+++ test/01_about_streams.js
@@ -1,58 +1,58 @@
-var Rx = require('rx'),
-    Observable = Rx.Observable,
-    Subject = Rx.Subject,
-    Range = require('../util/range');
+import Rx from 'rx';
+import Range from '../util/range';
+
+const {Observable, Subject} = Rx;
 
 QUnit.module('Observable Streams');
 
-var __ = 'Fill in the blank';
+const __ = 'Fill in the blank';
 
 test('simple subscription', function () {
-  Observable.just(42).subscribe(function (x) { equal(x, __); });
+  Observable.just(42).subscribe(function (x) { equal(x, 42); });
 });
 
 test('what comes in goes out', function () {
-  Observable.just(__).subscribe(function (x) { equal(x, 101); });
+  Observable.just(101).subscribe(function (x) { equal(x, 101); });
 });
 
 // Which interface Rx apply? (hint: what does "just()" return)
 test('this is the same as an event stream', function () {
-  var events = new Subject();
-  events.subscribe(function (x) { equal(__, x); });
+  const events = new Subject();
+  events.subscribe(function (x) { equal(37, x); });
   events.onNext(37);
 });
 
 // What is the relationship between "this is the same as an event stream" and "simple subscription"?
 test('how event streams relate to observables', function () {
-  var observableResult = 1;
+  let observableResult = 1;
   Observable.just(73).subscribe(function (x) { observableResult = x; });
 
-  var eventStreamResult = 1;
-  var events = new Subject();
+  let eventStreamResult = 1;
+  const events = new Subject();
   events.subscribe(function (x) { eventStreamResult = x; });
-  events.__(73);
+  events.onNext(73);
 
   equal(observableResult, eventStreamResult);
 });
 
 // What does Observable.just() map to for a Subject?
 test('event streams have multiple results', function () {
-  var eventStreamResult = 0;
-  var events = new Subject();
+  let eventStreamResult = 0;
+  let events = new Subject();
   events.subscribe(function (x) { eventStreamResult += x; });
 
   events.onNext(10);
   events.onNext(7);
 
-  equal(__, eventStreamResult);
+  equal(17, eventStreamResult);
 });
 
 // What does Observable.just() map to for a Subject?
 test('simple return', function () {
-  var received = '';
+  let received = '';
   Observable.just('foo').subscribe(function (x) { received = x; });
 
-  equal(__, received);
+  equal('foo', received);
 });
 
 test('the last event', function () {
